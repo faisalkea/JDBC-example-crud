00b7d9cbb006c9831d6bdbcd0b919cff140a4bfb
faisaljarkass <fafj@kea.dk>
2018-08-09 20:53:24 +0200

    Initial commit

---
+++ src/main/java/jdbc/mysql/mvc/demo/JdbcMvcMySqlApplication.java
@@ -0,0 +1,13 @@
+package jdbc.mysql.mvc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcMvcMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcMvcMySqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jdbc/mysql/mvc/demo/JdbcMvcMySqlApplicationTests.java
@@ -0,0 +1,16 @@
+package jdbc.mysql.mvc.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JdbcMvcMySqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
